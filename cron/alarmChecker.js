5fdaad8786be95636e296ff795c413089fa879c7
Bugrahancobain <bugrahancoban1@gmail.com>
2025-06-21 12:37:23 +0300

    alarmChecker edit

--- cron/alarmChecker.js
+++ cron/alarmChecker.js
@@ -10,12 +10,16 @@ cron.schedule("* * * * *", async () => {
     await connectDB();
     const now = new Date();
     const hhmm = now.toTimeString().slice(0, 5); // örn: "14:30"
-
+    const nowMinutes = now.getHours() * 60 + now.getMinutes();
 
     const users = await User.find({ "alarms.time": hhmm });
 
     for (const user of users) {
-        const dueAlarms = user.alarms.filter((a) => a.time === hhmm);
+        const dueAlarms = user.alarms.filter((alarm) => {
+            const [hour, minute] = alarm.time.split(":").map(Number);
+            const alarmMinutes = hour * 60 + minute;
+            return Math.abs(alarmMinutes - nowMinutes) <= 5;
+        });
 
         let hasChanges = false;
 
